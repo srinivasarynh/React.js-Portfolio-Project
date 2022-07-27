107ed9f49825e005304bace55f497f5de6374e05
srinivas a r <61593110+srinivasarynh@users.noreply.github.com>
2022-07-27 19:26:54 +0530

    Add files via upload

---
+++ src/components/portfolioList/PortfolioList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./portfolioList.scss"
+
+export default function PortfolioList({title, id, active, setSelected}) {
+    return(
+        <li className={active ? "portfolioList active" : "portfolioList"} onClick={() => setSelected(id)}>
+            {title}
+        </li>
+    )
+}
